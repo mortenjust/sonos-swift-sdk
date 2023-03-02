302862688dbca47bf2ce77992d0b79a4b4a87037
Morten Just <morten.just@genstart.dk>
2023-03-02 18:10:44 +0100

    conform favorite and service to codable
    This is so we can use Transferable with them for drag and drop, and in the future cache them for speed

--- Sources/SonosSDK/Models/Favorite.swift
+++ Sources/SonosSDK/Models/Favorite.swift
@@ -42,5 +42,34 @@ public struct Favorite: Identifiable {
         }
         
     }
+}
 
+
+
+extension Favorite : Codable {
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, imageUrl, imageCompilation, service
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        description = try values.decode(String.self, forKey: .description)
+        imageUrl = try values.decode(URL.self, forKey: .imageUrl)
+        imageCompilation = try values.decode([URL].self, forKey: .imageCompilation)
+        service = try values.decode(Service.self, forKey: .service)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(description, forKey: .description)
+        try container.encode(imageUrl, forKey: .imageUrl)
+        try container.encode(imageCompilation, forKey: .imageCompilation)
+        try container.encode(service, forKey: .service)
+    }
+    
+    
 }
--- Sources/SonosSDK/Models/Service.swift
+++ Sources/SonosSDK/Models/Service.swift
@@ -25,3 +25,27 @@ public struct Service: Identifiable {
     }
 
 }
+
+
+extension Service : Codable {
+    enum CodingKeys : String, CodingKey {
+        case id, name, imageUrl
+    }
+    
+    public init(from decoder: Decoder) throws {
+        
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        imageUrl = try values.decode(String.self, forKey: .imageUrl)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(imageUrl, forKey: .imageUrl)
+        
+    }
+    
+}

bca7cef81bd60b77cb5b3dbbae87bb916b815efd
Morten Just <morten.just@genstart.dk>
2023-03-04 17:16:37 +0100

    Conform Favorite to transferable
    so it can be dragged and dropped

--- Sources/SonosSDK/Models/Favorite.swift
+++ Sources/SonosSDK/Models/Favorite.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import SwiftyJSON
+import CoreTransferable
+
 
 public struct Favorite: Identifiable {
     
@@ -47,29 +49,45 @@ public struct Favorite: Identifiable {
 
 
 extension Favorite : Codable {
-    enum CodingKeys: String, CodingKey {
-        case id, name, description, imageUrl, imageCompilation, service
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        id = try values.decode(String.self, forKey: .id)
-        name = try values.decode(String.self, forKey: .name)
-        description = try values.decode(String.self, forKey: .description)
-        imageUrl = try values.decode(URL.self, forKey: .imageUrl)
-        imageCompilation = try values.decode([URL].self, forKey: .imageCompilation)
-        service = try values.decode(Service.self, forKey: .service)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(description, forKey: .description)
-        try container.encode(imageUrl, forKey: .imageUrl)
-        try container.encode(imageCompilation, forKey: .imageCompilation)
-        try container.encode(service, forKey: .service)
-    }
-    
-    
+//    enum CodingKeys: String, CodingKey {
+//        case id, name, description, imageUrl, imageCompilation, service
+//    }
+//    
+//    public init(from decoder: Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        id = try values.decode(String.self, forKey: .id)
+//        name = try values.decode(String.self, forKey: .name)
+//        description = try values.decode(String.self, forKey: .description)
+//        imageUrl = try values.decode(URL.self, forKey: .imageUrl)
+//        imageCompilation = try values.decode([URL].self, forKey: .imageCompilation)
+//        service = try values.decode(Service.self, forKey: .service)
+//    }
+//    
+//    public func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(id, forKey: .id)
+//        try container.encode(name, forKey: .name)
+//        try container.encode(description, forKey: .description)
+//        try container.encode(imageUrl, forKey: .imageUrl)
+//        try container.encode(imageCompilation, forKey: .imageCompilation)
+//        try container.encode(service, forKey: .service)
+//    }
 }
+
+
+extension Favorite : Hashable {
+    public static func == (lhs: Favorite, rhs: Favorite) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(description)
+        hasher.combine(imageUrl)
+        hasher.combine(imageCompilation)
+        hasher.combine(service)
+    }
+}
+
+// Transferable extension is conformed to in the project itself as it needs a high sdk version and a project-specific UTType
